534a341d56df84dc38abc0288592732ee6e1ace2
sdrazdauskas <180286095+sdrazdauskas@users.noreply.github.com>
2024-11-29 22:10:18 +0200

    add Dockerfile

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hospital_db',
+        'USER': 'webuser',
+        'PASSWORD': 'b1KG2jpQsYAPdlPY',
+        'HOST': 'your_mysql_server_ip',  # IP address of the MySQL server
+        'PORT': '3306',  # Default MySQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
